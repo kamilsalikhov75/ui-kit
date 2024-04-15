678667db9f0ac93ce04d74f2852ce605093ebb1f
kasmil <kamilsalikhov75@gmail.com>
2024-04-15 23:53:01 +0300

    added icons

---
+++ src/packages/icons/arrows/index.ts
@@ -0,0 +1,7 @@
+export * from "./ArrowDownIcon";
+export * from "./ArrowDownLeftIcon";
+export * from "./ArrowDownRightIcon";
+
+export * from "./BoldArrowDownIcon";
+export * from "./BoldArrowDownLeftIcon";
+export * from "./BoldArrowDownRightIcon";
